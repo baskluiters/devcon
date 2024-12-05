32eb2bfb1e869cd0583cebf58447c31fb4e3fa4e
Bas Kluiters <bas.kluiters@capgemini.com>
2024-12-05 15:33:27 +0100

    Initial version which compiles, deploys and runs an application

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # devcon
 A demo on the usage of Development Containers in VS Code
+
+ssh-keygen -t rsa -b 4096
+ssh-copy-id pi@100.90.31.75
+ssh pi@100.90.31.75 "echo 'Connection successful'"
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// #include <chrono>
+// #include <thread>
+
+// int main() {
+//     int counter = 0;
+//     while (true) {
+//         std::cout << "Counter: " << counter++ << std::endl;
+//         std::this_thread::sleep_for(std::chrono::seconds(1));
+//     }
+//     return 0;
+// }
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Raspberry Pi!" << std::endl;
+    return 0;
+}
